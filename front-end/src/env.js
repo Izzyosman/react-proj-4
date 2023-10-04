832213a8deae0721e9295025257bb8be29439dc5
Izzyosman <137222951+Izzyosman@users.noreply.github.com>
2023-10-03 21:22:54 -0500

    Delete front-end/src/env.js

--- front-end/src/env.js
+++
@@ -1,5 +0,0 @@
-require('dotenv').config();
-export const env = {
-    apiKey: 'jnZqza1cS0hT1T-O3mhZfv9Gh0UpIxxL-gxf22MvZAcvxtj2Q5qPjZFh-ig7SFEhMS-ifZ1cpzecYyQcauVjVfWaNAJp48Zzrnf6OpjYWVdsCuGlBEaurkXCYAMOZXYx',
-    apiUrl: "https://api.yelp.com/v3/businesses",
-  };
