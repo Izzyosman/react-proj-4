2705db210b5b3fa424acd543dd365b912f8aaf67
Izzyosman <137222951+Izzyosman@users.noreply.github.com>
2023-10-03 21:18:52 -0500

    Update FavoritesList.js

--- front-end/src/components/FavoritesList.js
+++ front-end/src/components/FavoritesList.js
@@ -1,20 +1,74 @@
-import React from 'react';
-import SaveButton from './SaveButton'; // Import the SaveButton component
+import React, { useEffect, useState, useContext } from 'react';
+import AuthContext from '../store/authContext'
+import '../assets/style.css'
 
-const FavoritesList = ({ favorites, removeFromFavorites, addToFavorites }) => {
+const FavoritesList = () => {
+  const { state: { userId, token } } = useContext(AuthContext);
+  const [favorites, setFavorites] = useState([]);
+
+  useEffect(() => {
+    console.log(token)
+    fetch(`http://localhost:4005/api/favorites/`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': token
+      }
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        // console.log(data);
+        setFavorites(data);
+      })
+      .catch((error) => {
+        console.error('Error fetching favorites:', error);
+      });
+  }, [token]);
+
+  const removeFromFavorites = (id) => {
+    fetch(`http://localhost:4005/api/favorites/${id}`, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': token
+      }
+    })
+      .then((response) => response.json())
+      .then(() => {
+        // Remove the favorite from the local state for immediate feedback
+        setFavorites((prevFavorites) => prevFavorites.filter(favorite => favorite.id !== id));
+      })
+      .catch((error) => {
+        console.error('Error removing from favorites:', error);
+      });
+  };
+
+  if (favorites.length === 0) {
+    return (
+      <div>
+        <h2>Favorites</h2>
+        <p>No favorites added yet.</p>
+      </div>
+    );
+  }
+
   return (
-    <div>
+    <div className="center-content">
       <h2>Favorites</h2>
-      <ul>
-        {favorites.map((restaurant) => (
-          <li key={restaurant.id}>
-            {restaurant.name}
-            <button onClick={() => removeFromFavorites(restaurant)}>Remove</button>
-            {/* Include the SaveButton component */}
-            <SaveButton onClick={() => addToFavorites(restaurant)} />
-          </li>
+      <div className="business-list">
+        {favorites && favorites.map((favorite) => (
+          <div key={favorite.id} className='business-card'>
+            <div className="business-image">
+              <img src={favorite.imageUrl} alt={favorite.name} />
+            </div>
+            <div className="business-details">
+              <h2>{favorite.name}</h2>
+              <p>{favorite.address}</p>
+              <button onClick={() => removeFromFavorites(favorite.id)}>Remove from Favorites</button>
+            </div>
+          </div>
         ))}
-      </ul>
+      </div>
     </div>
   );
 };
