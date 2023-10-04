40157651316b1ffc0319af72235cf288e6a13772
Izzyosman <137222951+Izzyosman@users.noreply.github.com>
2023-10-03 21:19:47 -0500

    Update Home.js

--- front-end/src/components/Home.js
+++ front-end/src/components/Home.js
@@ -1,102 +1,105 @@
+import React from 'react';
 import {useState, useEffect, useContext} from 'react'
 import AuthContext from '../store/authContext'
 import searchYelp from '../api/searchYelp'
 import '../assets/style.css'
 
 const Home = () => {
-    const { state: { userId, token } } = useContext(AuthContext);
-    const [businesses, setBusinesses] = useState([]); // State to store Yelp businesses
-    const [hasSearched, setHasSearched] = useState(false);
-    const [location, setLocation] = useState('');
+  const { state: { userId, token } } = useContext(AuthContext);
+  const [businesses, setBusinesses] = useState([]);
+  const [hasSearched, setHasSearched] = useState(false);
+  const [location, setLocation] = useState('');
 
-    const addToFavorites = (business) => {
-      const favoriteData = {
-        businessId: business.id,
-        imageUrl: business.image_url,
-        name: business.name,
-        address: business.location.address1,
-      };
-  
-      fetch('http://localhost:4005/api/add-to-favorites', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ favoriteData, userId }), // Include the business data and user ID
+  const addToFavorites = (business) => {
+    if(!token) {
+      alert('You need to login!')
+      return;
+    }
+    console.log(business);
+    const favoriteData = {
+      businessId: business.id,
+      imageUrl: business.image_url,
+      name: business.name,
+      address: business.location.address1,
+    };   
+
+    console.log(favoriteData);
+
+    fetch(`http://localhost:4005/api/favorites/${userId}`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': token
+      },
+      body: JSON.stringify({ favoriteData, userId }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        alert('Successfully added to favorites!');
+        console.log('Added to favorites:', data);
+
       })
-        .then((response) => response.json())
+      .catch((error) => {
+        console.error('Error adding to favorites:', error);
+
+      });
+  };
+
+
+  const handleLocationSubmit = () => {
+    if (location) {
+      searchYelp(location)
         .then((data) => {
-          console.log('Added to favorites:', data);
-          // Optionally, you can update the UI to reflect that the restaurant has been added to favorites
+            console.log(data);
+            setHasSearched(true);
+            setBusinesses(data); // Update state with Yelp businesses
         })
         .catch((error) => {
-          console.error('Error adding to favorites:', error);
           // Handle error
         });
-    };
+    }
+  };
 
-
-    const handleLocationSubmit = () => {
-        if (location) {
-          searchYelp(location)
-            .then((data) => {
-                console.log(data);
-                setHasSearched(true);
-                setBusinesses(data); // Update state with Yelp businesses
-            })
-            .catch((error) => {
-              // Handle error
-            });
-        }
-    };
-
-    const businessCards = businesses?.map((business) => (
-        <div key={business.id} className='business-card'>
-          <div className="business-image">
-            <img src={business.image_url} alt={business.name} />
-          </div>
-          <div className="business-details">
-            <h2>{business.name}</h2>
-            <p>{business.location.address1}</p>
-            <button className="add-to-favorites-button">Add to Favorites</button>
-          </div>
-        </div>
-    ));
+  const businessCards = businesses && businesses.map((business) => (
+    <div key={business.id} className='business-card'>
+      <div className="business-image">
+        <img src={business.image_url} alt={business.name} />
+      </div>
+      <div className="business-details">
+        <h2>{business.name}</h2>
+        <p>{business.location.address1}</p>
+        <button 
+          className="add-to-favorites-button" 
+          onClick={() => addToFavorites(business)}>Add to Favorites</button>
+      </div>
+    </div>
+  ));
       
+  return (
+    <main>
+      <div>
+          <h1>Enter Zip Code to Search Restaurants</h1>
+          <br />
+          <input
+          type="text"
+          placeholder="Enter a zipcode..."
+          value={location}
+          onChange={(e) => setLocation(e.target.value)}
+          />
+          <button onClick={handleLocationSubmit}>Submit</button>
+      </div>
+      {hasSearched && (
+          <div className="business-list">
+          {businessCards &&businessCards.length >= 1 ? (
+              businessCards
+          ) : (
+              <p>No results found</p>
+          )}
+          </div>
+      )}
+    </main>
+  );
 
-    // ... (Previous code)
-
-    return (
-        <main>
-        <div>
-            <h1>Welcome! </h1>
-            <br /> 
-            <h1>This is a restaurant planner that manages your favorite restaurants.<h1></h1>
-   
-             Enter a zipcode to find restaurants! </h1>
-            <br />
-            <input
-            type="text"
-            placeholder="Enter a zipcode..."
-            value={location}
-            onChange={(e) => setLocation(e.target.value)}
-            />
-            <button onClick={handleLocationSubmit}>Submit</button>
-        </div>
-        {hasSearched && (
-            <div className="business-list">
-            {businessCards?.length >= 1 ? (
-                businessCards
-            ) : (
-                <p>No results found</p>
-            )}
-            </div>
-
-            
-        )}
-        </main>
-    );
-  
 }
 
 export default Home
